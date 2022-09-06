1f1f68aeaa9685e2b5be582466bce529cfbc7b7e
Rizky Maulana <rizki.maulana@happyfresh.com>
2022-09-06 22:16:09 +0700

    Avoid sql injection using query param

--- sql_test.go
+++ sql_test.go
@@ -13,9 +13,10 @@ func TestInsertSql(t *testing.T) {
 	defer db.Close()
 
 	ctx := context.Background()
-
-	script := "INSERT INTO customer(id,name) VALUES('2', 'kojek')"
-	_, err := db.ExecContext(ctx, script)
+	paramID := "1"
+	paramName := "kojek"
+	script := "INSERT INTO customer(id,name) VALUES($1, $2)"
+	_, err := db.ExecContext(ctx, script, paramID, paramName)
 	if err != nil {
 		panic(err)
 	}
@@ -31,6 +32,30 @@ func TestQuerySql(t *testing.T) {
 
 	script := "SELECT id, name FROM customer"
 	rows, err := db.QueryContext(ctx, script)
+	defer rows.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id, name string
+		rows.Scan(&id, &name)
+
+		fmt.Println("ID = ", id)
+		fmt.Println("Name = ", name)
+	}
+}
+
+func TestQuerySqlWithParam(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	paramID := "1"
+	paramName := "kojek"
+	script := "SELECT id, name FROM customer where id = $1 AND name = $2"
+	rows, err := db.QueryContext(ctx, script, paramID, paramName)
 	defer rows.Close()
 	if err != nil {
 		panic(err)
